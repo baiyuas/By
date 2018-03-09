aceb0a1769f24da5495a78bfb0d604039c254c6e
Leo <leoliu@baiyuas.com>
2018-03-10 07:47:50 +0800

    add login module

---
+++ app/src/main/java/com/baiyuas/utils/Utils.java
@@ -0,0 +1,17 @@
+package com.baiyuas.utils;
+
+/**
+ * @作者: Leo
+ * @时间:2018/3/10
+ * @描述:https://baiyuas.github.io/
+ */
+public class Utils {
+
+    public static boolean isValidEmail(String email) {
+        return email.contains("@");
+    }
+
+    public static boolean isValidPassword(String password) {
+        return password.length() > 4;
+    }
+}
